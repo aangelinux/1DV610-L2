a7de3506d230f822f7056fffb26e202cc950f2c0
Angelina Larsson <al227bn@student.lnu.se>
2025-09-29 15:25:20 +0200

    Add test CSS template to demo app

---
+++ test/testTemplate.js
@@ -0,0 +1,64 @@
+export const testTemplate = document.createElement('style')
+testTemplate.innerHTML = 
+`
+#chart {
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-template-rows: 1fr;
+    place-items: center;
+}
+
+#chartTitle {
+    grid-area: 1 / 1;
+    align-self: flex-start;
+    margin: 0;
+}
+
+#yAxis {
+    border-left: 2px solid black;
+    grid-area: 1 / 1;
+}
+
+#yAxisRow {
+    align-self: flex-start;
+    border-top: 1px solid black;
+    grid-area: 1 / 1;
+}
+
+#yAxisValue {
+    display: inline;
+    grid-area: 1 / 1;
+}
+
+#xAxis {
+    display: flex;
+    align-self: flex-end;
+    border-top: 2px solid black;
+    gap: 5px;
+    grid-area: 1 / 1;
+}
+
+#xAxisLabel {
+    text-align: center;
+    overflow: hidden;
+    grid-area: 1 / 1;
+    // word-wrap: break-word;
+}
+
+#svgGraph {
+    display: flex;
+    grid-area: 1 / 1;
+    z-index: 1;
+}
+
+#polyline {
+    fill: none;
+    stroke-width: 3;
+}
+
+#slice {
+    stroke: blue;
+    stroke-width: 2;
+}
+
+`
